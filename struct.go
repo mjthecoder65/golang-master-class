e0551b9e8c682540a16b3a1656cd73097d62739b
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-01 16:58:43 +0900

    Added struct.go

---
+++ struct.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+// Struct is used to create a compound data type.
+// It is used to group related data together.
+// It is similar to a class in OOP.
+// Data fields in a struct can have different data types.
+
+// Syntax:
+/*
+type structName struct {
+	field dataType
+	field dataType
+}
+*/
+
+type Account struct {
+	accountNumber int
+	balance       float64
+}
+
+// Struct methods.
+// A struct method is a function that is associated with a struct.
+// A struct method is similar to a method in OOP.
+
+// Syntax:
+/*
+func (structName structType) methodName() {
+	// Code goes here
+}
+*/
+
+func (account *Account) deposit(amount float64) {
+	account.balance += amount
+}
+
+func (account Account) getCurrentBalance() float64 {
+	return account.balance
+}
+
+func (account Account) withdraw(amount float64) {
+	account.balance -= amount
+}
+
+func (account Account) getAccountNumber() int {
+	return account.accountNumber
+}
+
+func testStruct() {
+
+	fmt.Println("Learning about structs.")
+
+	account := Account{accountNumber: 12345, balance: 1000.50}
+	fmt.Println(account.accountNumber)
+	fmt.Println(account.accountNumber)
+}
