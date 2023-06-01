85a9b1b43c21e4a85c260873079ac046107ab25a
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-01 17:11:55 +0900

    Finished learning constants

---
+++ constants.go
@@ -0,0 +1,28 @@
+package main
+
+// constant is a name of or identifier for a fixed value.
+// Constants are immutable.
+// Keyword const is used to declare a constant.
+// You must assign a value to a constant when you declare it, you cannot assign later as with variables.
+
+// Syntax: const CONSTANT_NAME dataType = value
+// Example: const PI float64 = 3.14159
+
+// Naming convention for constants:
+// 1. Use all upper case letters.
+// 2. Use underscore to separate words.
+
+func testingConstants() {
+	// Declaring a constant
+	const NUMBER_OF_ELEMENTS int = 10
+
+	var products [NUMBER_OF_ELEMENTS]string
+	products[0] = "Computer"
+
+	// Declaring multiple constants using parentheses.
+	const (
+		PI       float64 = 3.14159
+		LANGUAGE string  = "Go"
+	)
+
+}
