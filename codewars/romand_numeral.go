54e6752f03e16c21de65e599f6b8c3f28396b471
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-16 08:49:42 +0900

    Renamed the function for spliting string

--- codewars/romand_numeral.go
+++
@@ -1,8 +0,0 @@
-package codewars
-
-// Question: https://www.codewars.com/kata/51b62bf6a9c58071c600001b/go
-
-func ToRoman(number int) string {
-
-	return ""
-}
--- codewars/split_strings.go
+++ codewars/split_strings.go
@@ -2,32 +2,7 @@ package codewars
 
 // Question: https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/go
 
-func Solution(str string) []string {
-	// Solution I created myself.
-	var temp string
-
-	result := make([]string, 0)
-
-	for _, char := range str {
-
-		if len(temp) == 2 {
-			result = append(result, temp)
-			temp = ""
-		}
-
-		temp += string(char)
-	}
-
-	if len(temp) == 1 {
-		temp = temp + "_"
-	}
-
-	result = append(result, temp)
-
-	return result
-}
-
-func Solution2(str string) []string {
+func SplitString(str string) []string {
 	result := make([]string, 0)
 
 	if len(str)%2 != 0 {
