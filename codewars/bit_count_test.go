e1ef0fcc7453ccb9c7538e716ac065caba1f880b
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-16 08:27:55 +0900

    Added unit test for CountBits function

---
+++ codewars/bit_count_test.go
@@ -0,0 +1,14 @@
+package codewars
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBitCount(t *testing.T) {
+	require.Equal(t, 0, CountBits(0))
+	require.Equal(t, 1, CountBits(4))
+	require.Equal(t, 5, CountBits(1234))
+	require.Equal(t, 2, CountBits(7))
+}
