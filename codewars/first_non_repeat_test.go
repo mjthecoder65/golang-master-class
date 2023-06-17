d0813afb089b99c6e68e2c2ba166e25e6bf84181
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-17 16:18:50 +0900

    Updated first_non_repeat_test.go

--- codewars/first_non_repeat_test.go
+++ codewars/first_non_repeat_test.go
@@ -11,4 +11,5 @@ func TestFirstNonRepeating(t *testing.T) {
 	require.Equal(t, "t", FirstNonRepeating("stress"))
 	require.Equal(t, "e", FirstNonRepeating("moonmen"))
 	require.Equal(t, ",", FirstNonRepeating("Go hang a salami, I'm a lasagna hog!"))
+	require.Equal(t, "", FirstNonRepeating(""))
 }
