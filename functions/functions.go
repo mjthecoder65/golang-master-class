b96057c8d716c2a1f536504672fec32db92c162b
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-20 22:40:13 +0900

    Added functions package

---
+++ functions/functions.go
@@ -0,0 +1,35 @@
+package functions
+
+import "fmt"
+
+// Functions are the building blocks of a Go program.
+// Functions are used to organize code.
+// Functions are used to make code more readable.
+// Functions are used to make code reusable.
+// Functions are used to make code more maintainable.
+// Functions are first-class in go.
+
+// Variadic functions
+// Variadic functions are functions that take a variable number of arguments.
+
+// Syntax:
+/*
+func functionName(arg ...type) returnType {
+	// code
+}
+*/
+// The three dots (...) are called the ellipsis operator.
+
+func Sum(numbers ...int) int {
+	var total int
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func Println(words ...string) {
+	for _, word := range words {
+		fmt.Println(word)
+	}
+}
