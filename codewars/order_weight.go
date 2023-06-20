ae7cf852ad06c844422e6fb3f4972b99ba327c23
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-20 22:40:39 +0900

    Solved order weight problem

---
+++ codewars/order_weight.go
@@ -0,0 +1,28 @@
+package codewars
+
+import (
+	"sort"
+	"strings"
+)
+
+func SumDigits(number string) int {
+	var sum int
+	for _, v := range number {
+		sum += int(v - '0')
+	}
+	return sum
+}
+
+func OrderWeight(strng string) string {
+	words := strings.Fields(strng)
+
+	sort.Slice(words, func(i int, j int) bool {
+		if diff := SumDigits(words[i]) - SumDigits(words[j]); diff == 0 {
+			return words[i] < words[j]
+		} else {
+			return diff < 0
+		}
+	})
+
+	return strings.Join(words, " ")
+}
