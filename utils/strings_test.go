b4fbd88dc571390f1b74a210e51f5b64beda7503
Michael Jordan <mjthecoder53161@gmail.com>
2023-06-20 10:09:43 +0900

    Learned about strings functions

--- utils/strings_test.go
+++ utils/strings_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,4 +11,17 @@ func TestSplitString(t *testing.T) {
 	require.Equal(t, SplitString("Hello World"), []string{"Hello", "World"})
 	require.Equal(t, SplitString("  2000	10003 1234000   44444444   9999 11 11 22 123  "), []string{"2000", "10003", "1234000", "44444444", "9999", "11", "11", "22", "123"})
 
+}
+
+func TestContains(t *testing.T) {
+	require.True(t, strings.Contains("Hello", "ll"))
+	require.False(t, strings.Contains("Hello", "lll"))
+	require.True(t, strings.Contains("Hello", "He"))
+}
+
+func TestJoin(t *testing.T) {
+	require.Equal(t, strings.Join([]string{"Hello", "World"}, " "), "Hello World")
+	require.Equal(t, strings.Join([]string{"Hello", "World"}, ""), "HelloWorld")
+	require.Equal(t, strings.Join([]string{"Hello", "World"}, "-"), "Hello-World")
+	require.Equal(t, strings.Join([]string{}, " "), "")
 }
