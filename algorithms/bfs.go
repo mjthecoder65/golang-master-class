01bfde627972480ff5e58ef3924a541471620381
Michael Jordan <mjthecoder53161@gmail.com>
2023-10-02 16:54:33 +0900

    Added BFS and DFS algorithms

--- algorithms/bfs.go
+++ algorithms/bfs.go
@@ -1,1 +1,24 @@
 package algorithms
+
+// TODO: To be updated soon.....
+func BFS(root Node) {
+	// Create a queue to store the nodes that need to be visited.
+	queue := []Node{root}
+
+	// While the queue is not empty.
+	for len(queue) > 0 {
+		// Pop a node from the queue.
+		currentNode := queue[0]
+		queue = queue[1:]
+
+		// Mark the node as visited.
+		currentNode.Visited = true
+
+		// Visit the node's children.
+		for _, child := range currentNode.Edges {
+			if !child.Visited {
+				queue = append(queue, child)
+			}
+		}
+	}
+}
--- algorithms/dfs.go
+++ algorithms/dfs.go
@@ -1,1 +1,30 @@
 package algorithms
+
+type Node struct {
+	Value   int
+	Edges   []Node
+	Visited bool
+}
+
+// TODO: To be updated soon....
+func DFS(root Node) {
+	// Create a stack to store the nodes that need to be visited.
+	stack := []Node{root}
+
+	// While the stack is not empty.
+	for len(stack) > 0 {
+		// Pop a node from the stack.
+		currentNode := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// Mark the node as visited.
+		currentNode.Visited = true
+
+		// Visit the node's children.
+		for _, child := range currentNode.Edges {
+			if !child.Visited {
+				stack = append(stack, child)
+			}
+		}
+	}
+}
