09b3fe3b1c03700a8981297e3c56e775cb23884e
Michael Jordan <mjthecoder53161@gmail.com>
2023-10-02 22:09:57 +0900

    Updated solution for reverse_words

--- leetcode/reverse_words.go
+++ leetcode/reverse_words.go
@@ -1,7 +1,5 @@
 package leetcode
 
-import "strings"
-
 // October 1, 2023
 // URL : https://leetcode.com/problems/reverse-words-in-a-string-iii/
 
@@ -14,25 +12,48 @@ import "strings"
 // 	return result.String()
 // }
 
-func reverseWord(word string) string {
-	runes := []rune(word)
-	left, right := 0, len(runes)-1
+// func reverseWord(word string) string {
+// 	runes := []rune(word)
+// 	left, right := 0, len(runes)-1
 
-	for left < right {
-		runes[left], runes[right] = runes[right], runes[left]
-		left++
-		right--
-	}
+// 	for left < right {
+// 		runes[left], runes[right] = runes[right], runes[left]
+// 		left++
+// 		right--
+// 	}
 
-	return string(runes)
-}
+// 	return string(runes)
+// }
+
+// func ReverseWords(s string) string {
+// 	words := strings.Split(s, " ") // O(n)
+
+// 	for index, word := range words {
+// 		words[index] = reverseWord(word) // o(1)
+// 	}
+
+// 	return strings.Join(words, " ") // O(n)
+// }
 
 func ReverseWords(s string) string {
-	words := strings.Split(s, " ") // O(n)
+	runes := []rune(s)
+	left := 0
+	for right := 0; right < len(runes); right++ {
+		if runes[right] == ' ' || right == len(runes)-1 {
+			temp_left, temp_right := left, right-1
 
-	for index, word := range words {
-		words[index] = reverseWord(word) // o(1)
+			if right == len(runes)-1 {
+				temp_right = right
+			}
+
+			for temp_left < temp_right {
+				runes[temp_left], runes[temp_right] = runes[temp_right], runes[temp_left]
+				temp_left++
+				temp_right--
+			}
+
+			left = right + 1
+		}
 	}
-
-	return strings.Join(words, " ") // O(n)
+	return string(runes)
 }
