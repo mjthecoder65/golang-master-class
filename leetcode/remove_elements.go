2f6591fc396988c775ab6c71f0820f304e0806ac
Michael Jordan <mjthecoder53161@gmail.com>
2023-10-03 07:40:55 +0900

    Improved solution for remove element

--- leetcode/remove_elements.go
+++ leetcode/remove_elements.go
@@ -3,20 +3,32 @@ package leetcode
 // URL: https://leetcode.com/problems/remove-element/description/
 // Title: Remove Element
 
+// func RemoveElement(nums []int, val int) int {
+// 	remainingElements := []int{}
+
+// 	for _, num := range nums {
+// 		if num != val {
+// 			remainingElements = append(remainingElements, num)
+// 		}
+// 	}
+
+// 	// for i := 0; i < len(remainingElements); i++ {
+// 	// 	nums[i] = remainingElements[i]
+// 	// }
+
+// 	// return len(remainingElements)
+
+// 	return copy(nums, remainingElements)
+// }
+
 func RemoveElement(nums []int, val int) int {
-	remainingElements := []int{}
+	overWriteIndex := 0 // The next index to overwrite.
 
 	for _, num := range nums {
 		if num != val {
-			remainingElements = append(remainingElements, num)
+			nums[overWriteIndex] = num
+			overWriteIndex++
 		}
 	}
-
-	// for i := 0; i < len(remainingElements); i++ {
-	// 	nums[i] = remainingElements[i]
-	// }
-
-	// return len(remainingElements)
-
-	return copy(nums, remainingElements)
+	return overWriteIndex
 }
