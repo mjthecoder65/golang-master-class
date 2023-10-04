140943af768b0e044216d66fc40cdfb82d3e74ac
Michael Jordan <mjthecoder53161@gmail.com>
2023-10-04 22:34:37 +0900

    Implemented hashmap using go array

--- dsa/hash_table.go
+++ dsa/hash_table.go
@@ -1,1 +1,38 @@
 package dsa
+
+// URL: https://leetcode.com/problems/design-hashmap/
+
+// Title: Design HashMap
+
+const MAP_CAPACITY = 1000000
+
+type HashMap struct {
+	elements []int
+}
+
+func Hash(key int) int {
+	return key % MAP_CAPACITY
+}
+
+func (hashMap *HashMap) Put(key int, value int) {
+	key = Hash(key)
+	hashMap.elements[key] = value
+}
+
+func (hashMap *HashMap) Get(key int) int {
+	key = Hash(key)
+	return hashMap.elements[key]
+}
+
+func (hashMap *HashMap) Remove(key int) {
+	key = Hash(key)
+	hashMap.elements[key] = -1
+}
+
+func Constructor() HashMap {
+	hashMap := HashMap{make([]int, MAP_CAPACITY)}
+	for index := range hashMap.elements {
+		hashMap.elements[index] = -1
+	}
+	return hashMap
+}
