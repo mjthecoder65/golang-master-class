db133c07180d8e24055cfbdf43a2452fcd7364b5
Michael <mjthecoder53161@gmail.com>
2024-08-01 13:10:00 +0900

    Add package for error handling

---
+++ errors/errors.go
@@ -0,0 +1,42 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+/*
+	Go does not raise error, instead functions return error of type
+		type error interface {
+			Error() string
+		}
+
+	error can be returned as nil or using errors.New() to construct basic error message
+
+	type errors struct {
+		Message String
+	}
+
+	func (error *errors) New (message string) *errors {
+		error.Message = message
+		return errors{Message: message}
+	}
+*/
+
+func Sqrt(number float64) (float64, error) {
+	if number < 0 {
+		return 0, errors.New("math: numbers must be greater than zero")
+	}
+	return math.Sqrt(number), nil // Always result first, and then error next.
+}
+
+func LearnErrorHandling() {
+
+	result, err := Sqrt(9)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
