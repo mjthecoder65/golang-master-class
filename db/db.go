8f24609743e8c4bf5837bc3ca33317845f3079f8
Michael <mjthecoder53161@gmail.com>
2024-08-04 10:48:10 +0900

    Add pq driver

---
+++ db/db.go
@@ -0,0 +1,13 @@
+package db
+
+/*
+ Learning about how to connect and interacting with Databases.
+ I will use Postgreql
+  Requirements:
+	1. pq driver: A driver for go to interact with PostgreSQL
+	2. go get github.com/lib/pq
+*/
+
+func LearnDBConnection() {
+
+}
